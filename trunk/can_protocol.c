7182284ae077ea153dc9f8fac3f5e0488ec93567
hashzhang2010@gmail.com <hashzhang2010@gmail.com@8835c150-c6c5-483a-8e51-0341df667f48>
2011-09-22 09:51:20 +0000

    1、修改NMT Module Control 发送函数

--- trunk/can_protocol.c
+++ trunk/can_protocol.c
@@ -4,9 +4,10 @@ void SendNMTModuleControl(NMT_MODULE_CONTROL NmtCmd,NODEID NodeID)
 {
     CanMsgTypeDef CanBuffer;
     CanBuffer.id =CANID_NMT_MODULE_CONTROL;
-    CanBuffer.dlc =2;
+    CanBuffer.dlc =3;
     CanBuffer.data[0]=NmtCmd;
-    CanBuffer.data[1]=NodeID;
+    CanBuffer.data[1]=(NodeID>>8)&0xFF;
+    CanBuffer.data[2]=NodeID&0xFF;
     SendToCan(&CanBuffer);
 }
 
